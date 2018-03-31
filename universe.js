2eaf7009b9238708b0dab867482692be67d7c235
Hoang Phan Trung Hieu <hieuhpt@deha-soft.com>
2018-03-31 19:32:27 +0700

    energize the universe

--- universe.js
+++ universe.js
@@ -19,6 +19,7 @@ let UniverseModel = (function () {
     };
 
     model.toHtmlString = function () {
+        model.universeString = '';
         for (let i = 0; i < model.x; i++) {
             model.universeString += `<div class="grid">`;
             for (let j = 0; j < model.y; j++) {
@@ -29,6 +30,46 @@ let UniverseModel = (function () {
         }
 
         return model.universeString;
+    };
+
+    model.evolve = function (time = 1) {
+        for (let i = 1; i <= time; i++) {
+            model.nextTime();
+        }
+    };
+
+    model.nextTime = function () {
+        let nextUniverse = this;
+        for (let i = 0; i < nextUniverse.x; i++) {
+            for (let j = 0; j < nextUniverse.y; j++) {
+                nextUniverse.calculateCellState(i, j, model.countNeighborPlanet(i, j));
+            }
+        }
+
+        model.universeMapNumber = nextUniverse.universeMapNumber;
+        return model.toHtmlString();
+    };
+
+    model.calculateCellState = function (x, y, numberOfNeighbor) {
+        if (numberOfNeighbor < 2 || numberOfNeighbor > 3) {
+            model.universeMapNumber[x][y] = 0;
+        } else if (3 === numberOfNeighbor) {
+            model.universeMapNumber[x][y] = 1;
+        }
+    };
+
+    model.countNeighborPlanet = function (i, j) {
+        return model.getValueOfCell(i - 1, j - 1) + model.getValueOfCell(i - 1, j) + model.getValueOfCell(i - 1, j + 1)
+            + model.getValueOfCell(i, j - 1) + model.getValueOfCell(i, j + 1)
+            + model.getValueOfCell(i + 1, j - 1) + model.getValueOfCell(i + 1, j) + model.getValueOfCell(i + 1, j + 1);
+    };
+
+    model.getValueOfCell = function (x, y) {
+        if (x < 0 || x >= model.x || y < 0 || y >= model.y) {
+            return 0;
+        }
+
+        return model.universeMapNumber[x][y];
     };
 
     return model;
